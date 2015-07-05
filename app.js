ab49314ed05bc79748edccda894cdf84c8f155a5
Jake A Barnwell <jb16@mit.edu>
2015-07-05 17:10:11 -0400

    Modifying overall control flow for filter logic

--- app.js
+++ app.js
@@ -21,24 +21,138 @@ var SUBSCRIBER = "Subscriber",
 	ADMIN = "Admin",
 	STAFF = "Staff";
 
-var POWER = [MODERATOR, BROADCASTER, GLOBALMODERATOR, ADMIN, STAFF]
+var POWER = [MODERATOR, BROADCASTER, GLOBALMODERATOR, ADMIN, STAFF];
+var OPTIONS = {
+	staffMessagePriority: true,
+	notificationPriority: true,
+	lengthRestrict: true,
+	byAccountStatus: true,
+	triggerPhrase: true,
+	copyPasta: true
+
+}
+
+var ALL_POSSIBLE_FILTERS = {
+	staffMessagePriority: {
+		filter: filter_byStaff,
+		priority: 1
+	},
+	notificationPriority: {
+		filter: filter_notification,
+		priority: 2
+	},
+	lengthRestrict: {
+		filter: filter_lengthRestrict,
+		priority: 3
+	},
+	byAccountStatus: {
+		filter: filter_byAccountStatus,
+		priority: 4
+	},
+	triggerPhrase: {
+		filter: filter_triggerPhrase,
+		priority: 5
+	},
+	copyPasta: {
+		filter: filter_copyPasta,
+		priority: 6
+	}
+
+}
+
+var unorderedFilters = {};
+// if(OPTIONS.staffMessagePriority) {
+// 	unorderedFilters.staffMessagePriority = ALL_POSSIBLE_FILTERS.staffMessagePriority;
+// }
+// if(OPTIONS.notificationPriority) {
+// 	unorderedFilters.notificationPriority = ALL_POSSIBLE_FILTERS.notificationPriority;
+// }
+// if(OPTIONS.lengthRestrict) {
+// 	unorderedFilters.lengthRestrict = ALL_POSSIBLE_FILTERS.lengthRestrict;
+// }
+// if(OPTIONS.byAccountStatus) {
+// 	unorderedFilters.byAccountStatus = ALL_POSSIBLE_FILTERS.byAccountStatus;
+// }
+// if(OPTIONS.triggerPhrase) {
+// 	unorderedFilters.triggerPhrase = ALL_POSSIBLE_FILTERS.triggerPhrase;
+// }
+// if(OPTIONS.copyPasta) {
+// 	unorderedFilters.copyPasta = ALL_POSSIBLE_FILTERS.copyPasta;
+
+// Addes the appropriate filters into our active filter buffer
+for (var key in OPTIONS) {
+   	if (OPTIONS.hasOwnProperty(key)) {
+   		if(OPTIONS[key] === true) {
+   			unorderedFilters[key] = ALL_POSSIBLE_FILTERS[key];
+   		}
+    }
+}
+
+// this is a special jquery selection set
+var node_arr_badge = node_badges.children(".badge");
+var badges = [];
+for(var i = 0; i < node_arr_badge.length; i++) {
+	badges.push($(node_arr_badge[i]).attr("title"));
+}
+
+
+text = text.toLowerCase();
+
+// if it's a notification, never delete it:
+if(notification_p(node_target)) {
+	return false;
+}
+
+// overrides all rules so let's put it first:
+staffPriority = true;
+if(staffPriority && byStaff(badges)) {
+	return false;
+}
+
+// if any of these are satisfied, we delete. 
+
+// Order least to most expensive in computation.
+lengthRestrict = false;
+if(lengthRestrict && isTooLong(text)) {
+	return true;
+}
+// they have the option to hide messages from plebs or from
+//  subs (if the subs have no other authorizations)
+byAccountStatus = false;
+if(byAccountStatus && wrongAccountType(badges)) {
+	return true;
+}
+trigger = true;
+if(trigger && hasTriggerPhrase(stripEmotes(text))) {
+	return true;
+}
+copyPasta = true;
+if(copyPasta && isCopyPasta(reduceEmotes(text))) {
+	return true;
+}
+
+return false;
+
+
 
 $(".chat-lines").bind("DOMNodeInserted", function(e) {
-	var target = $(e.target);
-	if(target.is("li")) {
-		var text = target.children(".message").html();
-		var badges = target.children(".badges");
+	var node_target = $(e.target);
+	if(node_target.is(".ember-view")) {
+		// var chat_line = node_target.children(".chat-line");
+		// var text = chat_line.children(".message").html();
+		// var badges = chat_line.children(".badges");
 
-		if(shouldBeDeleted(text, badges, target)) {
-			handle_deleteMessage(target);
+		if(shouldBeDeleted(node_target)) {
+			handle_deleteMessage(node_target);
 		}
+		// handle_deleteMessage(node_target);
 		
 	}
 });
 
 showDeletedMsgMarker = false;
-function handle_deleteMessage(target) {
-	target.css("background-color", "red");
+function handle_deleteMessage(node_target) {
+	node_target.css("background-color", "red");
 
 	if(showDeletedMsgMarker) {
 		$(".chat-lines").append("</hr style=\"{color: red}\">");
@@ -47,7 +161,7 @@ function handle_deleteMessage(target) {
 	}
 }
 
-function shouldBeDeleted(text, node_badges, node_target) {
+function shouldBeDeleted(node_target) {
 	if(!text) return false;
 
 	// this is a special jquery selection set
@@ -58,7 +172,7 @@ function shouldBeDeleted(text, node_badges, node_target) {
 	}
 	
 
-	text = text.toLowerCase()
+	text = text.toLowerCase();
 
 	// if it's a notification, never delete it:
 	if(notification_p(node_target)) {
@@ -100,6 +214,7 @@ function notification_p(target) {
 	return target.hasClass("notification");
 }
 
+// yeah yeah yeah, I know HTML isn't regular
 var emote_regex_str = "< *img *class=\\\".*\\\" *src=\\\".*\\\" *alt=\\\".*\\\" *title=\\\".*\\\" *>";
 function reduceEmotes(text) {
 	var emote = new RegExp(emote_regex_str, "gi");
@@ -243,6 +358,7 @@ function isAlreadyLogged(text) {
 	return false;
 }
 
+// Copy pasta distance
 function CP_distance(key, text) {
 	return text_distance(key, text);
 }
@@ -251,6 +367,7 @@ function text_distance(t1, t2) {
 	return DL_distance(t1, t2);
 }
 
+// D-Levenshtein Distance
 function DL_distance(a, b) {
 	var lenA = a.length;
 	var lenB = b.length;
